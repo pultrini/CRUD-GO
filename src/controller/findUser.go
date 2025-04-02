08ec6347c21d001e67150038f020095fa5b489e9
Davi <davi.pultrini@usp.br>
2025-04-01 22:11:57 -0300

    feat:criado as rotas

---
+++ src/controller/findUser.go
@@ -0,0 +1,12 @@
+package controller
+
+import "github.com/gin-gonic/gin"
+
+
+func FindUserByID(c *gin.Context){
+	
+}
+
+func FindUserByEmail(c *gin.Context){
+
+}
---
+++ src/controller/updateUser.go
@@ -0,0 +1,8 @@
+package controller
+
+import "github.com/gin-gonic/gin"
+
+
+func UpdateUser(c *gin.Context){
+	
+}
